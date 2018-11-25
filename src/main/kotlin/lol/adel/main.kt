b72bd486afb411eb3b9af7e1a4fb1c3db77cd106
adelnizamutdinov <stiggpwnz@gmail.com>
2018-11-25 18:56:07 +0300

    proper wait

--- src/main/kotlin/lol/adel/main.kt
+++ src/main/kotlin/lol/adel/main.kt
@@ -1,29 +1,22 @@
 package lol.adel
 
 import com.squareup.moshi.Moshi
-import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.client.HttpClient
-import io.ktor.client.call.HttpClientCall
 import io.ktor.client.call.call
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.response.readText
 import io.ktor.features.AutoHeadResponse
 import io.ktor.features.PartialContent
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
 import io.ktor.http.content.LocalFileContent
-import io.ktor.http.content.OutgoingContent
-import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.coroutines.io.ByteWriteChannel
-import kotlinx.coroutines.io.copyAndClose
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 fun List<Format>.bestAndSmallestAudio(): Format? =
     filter { it.audioQuality == AudioQuality.AUDIO_QUALITY_MEDIUM }.minBy { it.bitrate.rate }
@@ -38,27 +31,6 @@ suspend fun HttpClient.bestAndSmallestAudio(json: Moshi, id: VideoId): Format? =
 
 object Config {
     val PORT = System.getenv("PORT")?.toInt() ?: 8080
-    val FFMPEG_PORT = PORT + 1
-}
-
-suspend fun proxy(from: HttpClientCall, to: ApplicationCall) {
-
-    val original = from.response
-    val modified = to.response
-
-    modified.status(original.status)
-    modified.header(HttpHeaders.AcceptRanges, value = "bytes")
-
-    to.respond(object : OutgoingContent.WriteChannelContent() {
-
-        override val contentType: ContentType? = ContentType.Audio.MPEG
-
-        override val contentLength: Long? = null
-
-        override suspend fun writeTo(channel: ByteWriteChannel) {
-            original.content.copyAndClose(channel)
-        }
-    })
 }
 
 fun main(args: Array<String>) {
@@ -81,9 +53,9 @@ fun main(args: Array<String>) {
                     output = FFMpegOutput.File(File("/tmp/file.mp3"))
                 ).toCommand()
 
-                Runtime.getRuntime().exec(ffmpeg).awaitMetadata()
-
-                call.respond(LocalFileContent(output))
+                if (Runtime.getRuntime().exec(ffmpeg).waitFor(1, TimeUnit.MINUTES)) {
+                    call.respond(LocalFileContent(output))
+                }
             }
         }
     }.start(wait = true)
